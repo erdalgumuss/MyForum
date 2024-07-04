bb0eb257f7c0293248d0244b52db88163b915594
erdalgumuss <erdalgumus777@gmail.com>
2024-07-04 16:41:56 +0300

    update

--- handlers/forum.go
+++ handlers/forum.go
@@ -1,1 +1,36 @@
-	
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"MyForum/models"
+)
+
+func CreateTopic(w http.ResponseWriter, r *http.Request) {
+	var topic models.Topic
+	err := json.NewDecoder(r.Body).Decode(&topic)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if err := models.DB.Create(&topic).Error; err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(topic)
+}
+
+func ListTopics(w http.ResponseWriter, r *http.Request) {
+	var topics []models.Topic
+	if err := models.DB.Find(&topics).Error; err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(topics)
+}
