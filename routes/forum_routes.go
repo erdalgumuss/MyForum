df6bcf81b50afe4f52c80b1f0823bd4996998fbb
Abruzzi96 <tiritoglumert@gmail.com>
2024-07-11 12:02:17 -0400

    editted

--- routes/forum_routes.go
+++ routes/forum_routes.go
@@ -13,9 +13,7 @@ func ForumRoutes(r *gin.Engine) {
 	r.POST("/create-post", handlers.CreatePost)
 	r.GET("/create-post", handlers.RenderCreatePostPage)
 	r.GET("/forum", handlers.ShowForumPage)
-	r.POST("/forum", handlers.HandleForumPage)
 	r.GET("/gallery", handlers.GalleryPage)
-	r.POST("/gallery", handlers.GalleryPage) // If needed for POST requests
 	r.GET("/rules", handlers.RulesPage)
 	r.POST("/rules", handlers.RulesPage)
 	r.POST("/comments", handlers.CreateComment)              // controllers yerine handlers
