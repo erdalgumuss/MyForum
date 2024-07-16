fdbc0257740e28ac1d15fe5b17a0b774f552ce58
Abruzzi96 <tiritoglumert@gmail.com>
2024-07-16 16:15:34 -0400

    userObjectJsonfix

--- models/user.go
+++ models/user.go
@@ -2,22 +2,50 @@ package models
 
 import (
 	"database/sql"
+	"encoding/json"
 	"time"
 )
 
+type JSONNullString struct {
+	sql.NullString
+}
+
+func (ns *JSONNullString) UnmarshalJSON(data []byte) error {
+	var s *string
+	if err := json.Unmarshal(data, &s); err != nil {
+		return err
+	}
+	if s != nil {
+		ns.Valid = true
+		ns.String = *s
+	} else {
+		ns.Valid = false
+	}
+	return nil
+}
+
+func (ns JSONNullString) MarshalJSON() ([]byte, error) {
+	if ns.Valid {
+		return json.Marshal(ns.String)
+	} else {
+		return json.Marshal(nil)
+	}
+}
+
 type User struct {
 	ID         int            `json:"id"`    // Unique identifier for the user
-	Email      string         `json:"email"` // Email of the user (nullable)
-	Name       sql.NullString `json:"name"`
-	Surname    sql.NullString `json:"surname"`
-	Username   sql.NullString `json:"username"` // Username of the user (nullable)
+	Email      string         `json:"email"` // Email of the user
+	Name       JSONNullString `json:"name"`
+	Surname    JSONNullString `json:"surname"`
+	Username   JSONNullString `json:"username"` // Username of the user
 	Password   string         `json:"password"`
 	CreatedAt  time.Time      `json:"created_at"`
 	UpdatedAt  time.Time      `json:"updated_at"`
-	GoogleID   sql.NullString `json:"googleid"`
+	GoogleID   JSONNullString `json:"googleid"`
 	GitHubID   sql.NullInt64  `json:"githubid"`
-	FacebookID sql.NullString `json:"facebookid"`
+	FacebookID JSONNullString `json:"facebookid"`
 }
+
 type GoogleUserInfo struct {
 	ID    string
 	Name  string
